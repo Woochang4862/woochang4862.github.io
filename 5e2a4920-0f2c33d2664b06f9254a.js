651ded69561f26ae5048d30a2e44e92f3091ea8d
Woochang4862 <woochang4862@gmail.com>
2022-07-10 14:35:05 +0000

    Updates

---
+++ 5e2a4920-0f2c33d2664b06f9254a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hoodie=self.webpackChunkgatsby_starter_hoodie||[]).push([[152],{3750:function(t,r,e){e.d(r,{jRj:function(){return o}});var n=e(1046);function o(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"circle",attr:{cx:"11",cy:"11",r:"8"}},{tag:"line",attr:{x1:"21",y1:"21",x2:"16.65",y2:"16.65"}}]})(t)}}}]);
+//# sourceMappingURL=5e2a4920-0f2c33d2664b06f9254a.js.map
